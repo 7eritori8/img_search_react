30084f80c73f54967bd50e7f71f319a346699d41
7eritori8 <61018410+7eritori8@users.noreply.github.com>
2022-09-29 09:43:05 +0900

    画像検索機能を修正

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import History from "./History";
 import "./Header.css";
 
@@ -9,8 +9,7 @@ const Header = (props) => {
     // ボタンクリックされたときに下記が発火。
     const addSearchTextArray = () => {
         if (text !== "") {
-            // inputに入力した内容を親コンポーネントに送る
-            props.setValueText(text);
+            // props.onSubmitAtHeader(text)
             // 配列に入力内容を格納するこの配列はHistory.jsに送られる
             setSearchTextArray((prev) => {
                 return [...prev, text]
@@ -18,9 +17,9 @@ const Header = (props) => {
             )
         }
     }
-    const randomImageShow = () => {
-        props.generateRandomURL();
-    }
+    useEffect(() => {
+
+    }, [text])
     return (
         <>
             <header>
@@ -30,20 +29,22 @@ const Header = (props) => {
                 <div className="header__right-area flex-align-center">
                     <div className="right-area__search mr-2">
                         <form action="." className="flex" autoComplete="off">
-                            <input type="text" name="" id="textform" placeholder="cat dog ..."
+                            <input type="text" name="" placeholder="cat dog ..."
                                 value={text} onChange={(event) => setText(event.target.value)}
                             />
                             <ul id="search__history">
                                 {/* 配列を渡す。 */}
                                 <History searchTextArray={searchTextArray} />
                             </ul>
-                            <button id="submitBtn" type="button" onClick={addSearchTextArray}>
+                            <button id="submitBtn" type="button" onClick={() => {
+                                props.onSubmitFromHeader(text)
+                            }}>
                                 <img src={`/images/tab_search.svg`} alt="search" />
                             </button>
                         </form>
                     </div>
                     <div className="right-area__random-btn mr-2">
-                        <button id="randomBtn" className="flex-align-center" onClick={randomImageShow}>
+                        <button id="randomBtn" className="flex-align-center" onClick={props.onRandomBtnClick}>
                             <img src={`/images/ei-random.svg`} alt="randomImage" />
                         </button>
                     </div>
