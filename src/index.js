6eea08948443bd54588a04f2f0efe5396614aa11
7eritori8 <61018410+7eritori8@users.noreply.github.com>
2022-09-29 12:41:16 +0900

    react-modalをアンインストール、muiをインストール

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import Main from './Main.js';
 import reportWebVitals from './reportWebVitals';
 import './common.css';
-import Modal from "react-modal"; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
